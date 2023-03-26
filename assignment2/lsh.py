52b06cfe7e7874b126b026a4f2d09e6e13fbd548
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-03-26 12:53:29 +0200

    add inverted index methods

--- assignment2/lsh.py
+++ assignment2/lsh.py
@@ -7,8 +7,6 @@ from pathlib import Path  # for paths of files
 import os  # for reading the input data
 import time  # for timing
 from sympy import randprime  # for random prime number
-from random import randint
-from math import floor
 import numpy as np
 
 # Global parameters
@@ -82,10 +80,13 @@ def naive():
     # Using triangular array to store the similarities, avoiding half size and similarities of i==j
     num_elems = int(len(docs_Sets) * (len(docs_Sets) - 1) / 2)
     similarity_matrix = [0 for x in range(num_elems)]
+    comp = 0
     for i in range(len(docs_Sets)):
         for j in range(i + 1, len(docs_Sets)):
+            comp += 1
             similarity_matrix[get_triangle_index(i, j, len(docs_Sets))] = jaccard(docs_Sets[i], docs_Sets[j])
 
+    print("Comparisons: " + str(comp))
     return similarity_matrix
 
 
@@ -107,7 +108,41 @@ def k_shingles():
     return list(docs_k_shingles)
 
 
-# METHOD FOR TASK 2
+# # METHOD FOR TASK 2
+# # Creates a signatures set of the documents from the k-shingles list
+# def signature_set(k_shingles):
+#     '''
+#     :param k_shingles: list of k-shingles of each document
+#     :return: list of signature sets of each document (inverse index)
+#     '''
+#     docs_sig_sets = []
+
+#     shingle_array = np.array(k_shingles[0])
+
+#     # Create a union of all the shingles
+#     for k_shingle in k_shingles:
+#         shingle_array = np.union1d(shingle_array, np.array(k_shingle))
+    
+#     # REMOVE
+#     print(shingle_array.size)
+    
+#     # Create dict for speed optimization
+#     shingle_dict = {}
+#     for index, shingle in enumerate(shingle_array):
+#         shingle_dict[shingle] = index
+
+#     length = len(shingle_array)
+#     # Create the signature sets
+#     for docs_shingle in k_shingles:
+#         signature = list(np.zeros(length, dtype=int))
+#         for shingle in docs_shingle:
+#             index = shingle_dict[shingle]
+#             signature[index] = 1
+#         docs_sig_sets.append(list(signature))
+
+#     return docs_sig_sets
+
+# METHOD FOR TASK 2, inverted index version
 # Creates a signatures set of the documents from the k-shingles list
 def signature_set(k_shingles):
     '''
@@ -121,33 +156,57 @@ def signature_set(k_shingles):
     # Create a union of all the shingles
     for k_shingle in k_shingles:
         shingle_array = np.union1d(shingle_array, np.array(k_shingle))
-    
+
     # Create dict for speed optimization
     shingle_dict = {}
     for index, shingle in enumerate(shingle_array):
         shingle_dict[shingle] = index
 
-    length = len(shingle_array)
     # Create the signature sets
     for docs_shingle in k_shingles:
-        signature = list(np.zeros(length, dtype=int))
+        signature = []
         for shingle in docs_shingle:
             index = shingle_dict[shingle]
-            signature[index] = 1
+            signature.append(index) 
         docs_sig_sets.append(list(signature))
 
     return docs_sig_sets
 
 
 # Helper function for TASK 3
-def make_a_hash_function(a, b, x, l):
-    return (a * x + b) % l
-
-
 def hash(a, b, x, N, p):
     return (a * x + b) %p % N
 
-# METHOD FOR TASK 3
+# # METHOD FOR TASK 3
+# # Creates the minHash signatures after simulation of permutations
+# def minHash(docs_signature_sets):
+#     '''
+#     :param docs_signature_sets: list of signature sets of each document 
+#     :return: list of minHash signatures of each document
+#     '''
+#     min_hash_signatures = []
+    
+#     permutations = parameters_dictionary['permutations']
+#     N = len(docs_signature_sets[0])
+
+#     min_hash_signatures = [[] for _ in range(len(docs_signature_sets))]
+
+#     for _ in range(permutations):
+#         a = random.randint(1, N)
+#         b = random.randint(1, N)
+#         p = randprime(N, 2*N)
+#         for signature_index, signature_set in enumerate(docs_signature_sets):
+#             min_hash = N
+#             for index, value in enumerate(signature_set):
+#                 if value != 0:
+#                     min_hash = min(min_hash, hash(a, b, index, N, p))
+            
+#             min_hash_signatures[signature_index] += min_hash,
+
+#     return min_hash_signatures
+
+
+# METHOD FOR TASK 3, inverted index
 # Creates the minHash signatures after simulation of permutations
 def minHash(docs_signature_sets):
     '''
@@ -157,7 +216,7 @@ def minHash(docs_signature_sets):
     min_hash_signatures = []
     
     permutations = parameters_dictionary['permutations']
-    N = len(docs_signature_sets[0])
+    N = len(naive_similarity_matrix)
 
     min_hash_signatures = [[] for _ in range(len(docs_signature_sets))]
 
@@ -167,14 +226,19 @@ def minHash(docs_signature_sets):
         p = randprime(N, 2*N)
         for signature_index, signature_set in enumerate(docs_signature_sets):
             min_hash = N
-            for index, value in enumerate(signature_set):
-                if value != 0:
-                    min_hash = min(min_hash, hash(a, b, index, N, p))
-            
+            for index in signature_set:
+                min_hash = min(min_hash, hash(a, b, index, N, p))
             min_hash_signatures[signature_index] += min_hash,
 
     return min_hash_signatures
 
+
+# Helper method for task 4
+def bucket_hash(a, b, c, x):
+    factor = 0
+    for i in x:
+        factor += a * i + c
+    return factor % b
 
 # METHOD FOR TASK 4
 # Hashes the MinHash Signature Matrix into buckets and find candidate similar documents
@@ -201,14 +265,18 @@ def lsh(m_matrix):
             
         m_matrix_banded.append(banded_signature)
 
+    comp = 0
+    a = random.randint(1, buckets)
+    c = random.randint(1, buckets)
     # one loop per band
     for band_index in range(len(m_matrix_banded[0])):   
         hash_buckets = {}
         # one loop per signature in the band
         for signature_index, signature in enumerate(m_matrix_banded):
             # simple hashing, taking the sum of the band and modding it by the number of buckets
-            band = sum(signature[band_index])
-            hash_value = band % buckets
+            # band = sum(signature[band_index])
+            # hash_value = band % buckets
+            hash_value = bucket_hash(a=a, b=buckets, c=c, x=signature[band_index])
             # add hash value into corresponding bucket
             if hash_value not in hash_buckets:
                 hash_buckets[hash_value] = [signature_index]
@@ -222,7 +290,10 @@ def lsh(m_matrix):
                     for j in range(i + 1, len(bucket)):
                         candidate_pair = (bucket[i], bucket[j])
                         if candidate_pair not in candidates:
+                            comp += 1
                             candidates.add(candidate_pair)
+
+    print("Comparisons: ", comp)
     return list(candidates)
 
 
@@ -303,8 +374,12 @@ def count_false_neg_and_pos(lsh_similarity_matrix, naive_similarity_matrix):
             positive += 1
         if candidate_similarity >= t and naive_similarity < t:
             false_positives += 1
-        elif candidate_similarity < t and naive_similarity >= t:
-            false_negatives += 1
+        
+    for similarity in naive_similarity_matrix:
+        if similarity >= t:
+            actual_positive += 1
+
+    false_negatives = actual_positive - (positive - false_positives)
 
     print("There were a total of " + str(positive) + " similarities above treshold value, out of which " + str(positive - false_positives) + " are true positive. \n")
 
